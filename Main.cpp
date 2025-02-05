0697c08d3b0d8b30180103e2eb72928c3f4153ce
Max <maxkoell@proton.me>
2025-02-05 17:35:02 +0100

    working Projekt - at least for the moment

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include "MyUtil.h"
+#include "add.h"
+
+int main()
+{
+	int result = add(17, 23);
+	print(result);
+	
+	return EXIT_SUCCESS;
+}
---
+++ lib/MyUtils/MyUtil.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "MyUtil.h"
+
+void print(int value)
+{
+	std::cout << "The value is: " << value << std::endl;
+}
